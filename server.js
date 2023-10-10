566265b13cde0f29f8e664ccb72acf8b4bb5797e
Alif_04 <alifnaufalh@gmail.com>
2023-10-10 10:47:32 +0700

    fix: server

--- server.js
+++ server.js
@@ -1,31 +1,21 @@
-<<<<<<< HEAD
 const express = require("express");
 const bookRoutes = require("./src/books/routes/books.routes");
 const userRoutes = require("./src/users/routes/users.routes");
-const authRoutes = require("./src/users/controller/user.controller");
 const { PrismaClient } = require("@prisma/client");
-=======
 const express = require('express');
-const bookRoutes = require('./src/books/routes/books.routes');
 const categoriesRoutes = require('./src/category/routes/categories.routes')
 const { PrismaClient } = require('@prisma/client');
 
 
 
->>>>>>> 88b995ec33f15b206fa0f0d54cf4c2148b6d9a9e
 
 const dbService = new PrismaClient();
 const app = express();
 app.use(express.json());
-<<<<<<< HEAD
 app.use("/books", bookRoutes);
 app.use("/users", userRoutes);
 // Menggunakan rute autentikasi
-app.use("/auth", authRoutes);
-=======
-app.use('/books', bookRoutes);
 app.use('/categories', categoriesRoutes)
->>>>>>> 88b995ec33f15b206fa0f0d54cf4c2148b6d9a9e
 
 // Menggunakan rute yang dilindungi
 async function runPrisma() {
